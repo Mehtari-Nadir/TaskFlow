28031faa305c1b0e74ef3abe521c755d2220ede1
nadir <nadirmehtaripr@gmail.com>
2024-04-20 17:18:36 +0100

    Add dialog with create board button for home

--- app/_components/CreateBoardBtn_home.tsx
+++ app/_components/CreateBoardBtn_home.tsx
@@ -1,11 +1,23 @@
+"use client"
+
 import { PlusCircledIcon } from "@radix-ui/react-icons";
+import CreateBoardDialog from "./CreateBoardDialog";
+import { useState } from "react";
 
 const CreateBoardBtn_home = () => {
+
+    const [open, setOpen] = useState(false);
+
     return (
-        <div className="w-[250px] h-[130px] bg-c-three flex items-center justify-center gap-x-1 rounded-md">
-            <PlusCircledIcon className="h-4 w-4" />
-            Create Board
-        </div>
+        <>
+            <div
+                onClick={() => setOpen(true)}
+                className="w-[250px] h-[130px] cursor-pointer bg-c-three flex items-center justify-center gap-x-1 rounded-md">
+                <PlusCircledIcon className="h-4 w-4" />
+                Create Board
+            </div>
+            <CreateBoardDialog open={open} setOpen={setOpen} />
+        </>
     );
 }
 
