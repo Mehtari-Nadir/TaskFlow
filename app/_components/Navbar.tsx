a48ec92c138636fc85e5e07881a276a285a4e7bc
yassinedownpourz <akramyassine.amari@univ-tlemcen.dz>
2024-05-14 19:45:40 +0100

    Refactor Navbar component and update styles

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -2,37 +2,28 @@
 
 import Image from "next/image";
 import Link from "next/link";
-
+import TaskFlowLogo from "/public/assets/taskflow-logo.svg";
 import ProfileAvatar from "./ProfileAvatar";
 import NavbarMenu from "./NavbarMenu";
 import CreateBoardBtn from "./CreateBoardBtn";
 
-import { FaSearch } from "react-icons/fa";
-import { Button } from "@/components/ui/button";
-
 const Navbar = () => {
     return (
-        <nav className="w-full bg-c-two flex items-center justify-between py-2 px-5 border-b-[1px]">
-            <div className="flex items-center gap-x-2">
-                <Link href={"/home"}>
-                    <Image
-                        alt="logo"
-                        width={45}
-                        height={45}
-                        src={'/assets/logo.svg'}
-                    />
+        <nav className="w-full flex items-center justify-between py-3 px-5 border-b-[1px]">
+            <div className="flex items-center gap-x-8">
+                <Link href="/home">
+                    <div className="flex items-center gap-x-2 transition-all duration-300 hover:scale-105">
+                        <Image src={TaskFlowLogo} alt="taskflow-logo" width={50} priority />
+                        <div className="font-bold text-lg md:text-xl">
+                            <span>Task</span>
+                            <span className="text-persianGreen">Flow</span>
+                        </div>
+                    </div>
                 </Link>
-                <span className="text-2xl font-bold mr-5 max-md:hidden">
-                    Task
-                    <span className="text-c-one">Flow</span>
-                </span>
                 <NavbarMenu />
                 <CreateBoardBtn />
             </div>
             <div className="flex items-center gap-x-5">
-                <Button variant={"outline"} size={"icon"}>
-                    <FaSearch className="h-4 w-4" />
-                </Button>
                 <ProfileAvatar image_url="https://github.com/shadcn.png" />
             </div>
         </nav>
