5874ae2df2e2abbabb72d133df56a1b93169a5f2
yassinedownpourz <akramyassine.amari@univ-tlemcen.dz>
2024-05-14 19:38:01 +0100

    Implement search functionality in BoardsContainer

--- app/_components/BoardsContainer.tsx
+++ app/_components/BoardsContainer.tsx
@@ -3,22 +3,44 @@
 import CreateBoardBtn_home from "./CreateBoardBtn_home";
 import { useBoardStore } from "../_providers/board-store-provider";
 import BoardCard from "./BoardCard";
+import { Input } from "@/components/ui/input";
+import { useMemo } from "react";
+import { Search } from "lucide-react";
 
 const BoardsContainer = () => {
 
-    const boards = useBoardStore(state => state.boards);
+    const { boards, searchTerm, setSearchTerm } = useBoardStore(state => ({
+        boards: state.boards,
+        searchTerm: state.searchTerm,
+        setSearchTerm: state.setSearchTerm
+    }));
+
+    const filteredBoards = useMemo(() =>
+        boards.filter(({ boardTitle }: TBoard) =>
+            boardTitle.toLowerCase().includes(searchTerm.toLowerCase())
+        ), [boards, searchTerm]);
+
+    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => setSearchTerm(e.target.value);
 
     return (
-        <div className="w-full flex items-start justify-start max-md:justify-center gap-3 flex-wrap p-10">
-            {
-                boards.map((board, index) => {
-                    return (
-                        <BoardCard key={index} {...board} />
-                    );
-                })
-            }
-            <CreateBoardBtn_home />
-        </div>
+        <>
+            <div className="flex justify-end mx-6 mt-6 relative">
+                <Input className="w-full md:w-1/3" placeholder="Search boards..." value={searchTerm} onChange={handleSearch} />
+                <Search size={20} className="absolute right-0 top-1/2 transform -translate-y-1/2 mr-3" />
+            </div>
+            <div className="w-full flex items-center justify-center">
+                <div className="w-full flex items-start justify-start max-md:justify-center gap-3 flex-wrap p-6">
+                    {
+                        filteredBoards.map((board, index) => {
+                            return (
+                                <BoardCard key={index} {...board} />
+                            );
+                        })
+                    }
+                    {(!!filteredBoards.length && !!searchTerm) || <CreateBoardBtn_home />}
+                </div>
+            </div>
+        </>
     );
 }
 
