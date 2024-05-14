735700d6e54331e8839a91fc9375d4ec356e2a5d
yassinedownpourz <akramyassine.amari@univ-tlemcen.dz>
2024-05-14 19:15:20 +0100

    Refactored HomePage to directly include BoardsContainer

--- app/(routes)/home/page.tsx
+++ app/(routes)/home/page.tsx
@@ -3,9 +3,7 @@ import BoardsContainer from "@/app/_components/BoardsContainer";
 const homePage = () => {
     return (
         <main className="w-full">
-            <div className="w-full flex items-center justify-center">
-                <BoardsContainer />
-            </div>
+            <BoardsContainer />
         </main>
     );
 }
