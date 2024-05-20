2ea2eb35b968091a0a961e0d9f7bca17083935e1
yassinedownpourz <akramyassine.amari@univ-tlemcen.dz>
2024-05-20 11:41:31 +0100

    Update BoardCard.tsx to include filtering animation

--- app/_components/BoardCard.tsx
+++ app/_components/BoardCard.tsx
@@ -1,8 +1,7 @@
-import { DropdownMenuIcon, DotsHorizontalIcon } from "@radix-ui/react-icons";
-import { Button } from "@/components/ui/button";
+"use client";
 import Link from "next/link";
 import BoardCardMenu from "./BoardCardMenu";
-
+import { AnimatePresence, motion } from "framer-motion"
 const BoardCard = ({ boardId, boardTitle, boardDescription }: { boardId: string, boardTitle: string, boardDescription?: string }) => {
 
     // "#2e3192, #0068c7, #009ce8, #00cff9, #1bffff",
@@ -12,24 +11,31 @@ const BoardCard = ({ boardId, boardTitle, boardDescription }: { boardId: string,
     // "#764ba2, #7657b4, #7364c6, #6e71d8, #667eea"
 
     return (
-        <div
-            className="flex items-start justify-between w-[250px] h-[130px] p-3 rounded-md"
-            style={{
-                backgroundImage: `linear-gradient(to right top, #764ba2, #7657b4, #7364c6, #6e71d8, #667eea`
-                // backgroundImage: `linear-gradient(to right top, ${colors[random]})`
-            }}
-        >
-            <Link
-                href={`/home/board/${boardId}`}
+        <AnimatePresence>
+            <motion.div
+                layout
+                animate={{ opacity: 1 }}
+                initial={{ opacity: 0 }}
+                exit={{ opacity: 0 }}
             >
-                <h1 className="text-lg font-bold truncate">{boardTitle}</h1>
-            </Link>
-            <BoardCardMenu
-                boardId={boardId}
-                boardDescription={boardDescription}
-                boardTitle={boardTitle}
-            />
-        </div>
+                <div className="flex items-start justify-between w-[250px] h-[130px] p-3 rounded-md shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(45,78,255,0.15)]"
+                    style={{
+                        backgroundImage: `linear-gradient(to right top, #764ba2, #7657b4, #7364c6, #6e71d8, #667eea`
+                        // backgroundImage: `linear-gradient(to right top, ${colors[random]})`
+                    }}>
+                    <Link
+                        href={`/home/board/${boardId}`}
+                    >
+                        <h1 className="text-lg font-bold truncate">{boardTitle}</h1>
+                    </Link>
+                    <BoardCardMenu
+                        boardId={boardId}
+                        boardDescription={boardDescription}
+                        boardTitle={boardTitle}
+                    />
+                </div>
+            </motion.div>
+        </AnimatePresence>
     );
 }
 
